6c349d3a942520194264d6527f0e0c942a2ef8ce
Ashley Vu <ashleyvuhoo@gmail.com>
2018-07-26 20:13:52 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,29 @@
-#write out your code here
-
-def least_coins(cents)
-
-#Code your answer here!
-
+puts "How many cents do you have?"
+total_coins = gets.strip
+ puts "Calculating how many and what type of coins needed to get #{total_coins}."
+def least_coins(total_coins)
+  total_coins = total_coins.to_i 
+  coins= {
+    :quarters => 0, 
+    :dimes => 0, 
+    :nickels => 0, 
+    :pennies => 0
+  }
+  while total_coins >= 25 
+  coins[:quarters] += 1 
+  total_coins = total_coins -25 
 end
+  while total_coins >= 10 
+  coins[:dimes] += 1 
+  total_coins = total_coins -10
+end 
+while total_coins >= 5 
+coins[:nickels] += 1 
+total_coins = total_coins -5 
+end 
+while total_coins>= 1 
+coins[:pennies] += 1 
+total_coins = total_coins - 1 
+end 
+end
+puts least_coins
